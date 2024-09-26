5527cc815de9b4a3260e0d6cced145a2da9e6a4d
salvadorrus <ntvkras@list.ru>
2024-09-26 18:27:03 +0300

    Fix MapSchema.

--- app/src/main/java/hexlet/code/MapSchema.java
+++ app/src/main/java/hexlet/code/MapSchema.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Map;
 import java.util.Objects;
 
-public class MapSchema<K, V> extends BaseSchema<Map<K, V>>{
+public class MapSchema<K, V> extends BaseSchema<Map<K, V>> {
 
     public MapSchema<K, V> required() {
         readPredicate("required", Objects::nonNull);
@@ -12,6 +12,15 @@ public class MapSchema<K, V> extends BaseSchema<Map<K, V>>{
 
     public MapSchema<K, V> sizeof(Integer number) {
         readPredicate("sizeof", value -> ((Map<?, ?>) value).size() == number);
+        return this;
+    }
+
+    public MapSchema<K, V> shape(Map<K, BaseSchema<V>> schema) {
+        readPredicate("shape", value -> schema.entrySet().stream().allMatch(item -> {
+                    Object object = ((Map<?, ?>) value).get(item.getKey());
+                    return item.getValue().isValid(object);
+                })
+        );
         return this;
     }
 }
