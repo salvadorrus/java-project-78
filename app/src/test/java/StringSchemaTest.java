2661c9f3113a873156e970b80d7c46b2b633525a
salvadorrus <ntvkras@list.ru>
2024-09-27 18:48:13 +0300

    Fix gradle

--- app/src/test/java/StringSchemaTest.java
+++ app/src/test/java/StringSchemaTest.java
@@ -1,16 +1,16 @@
-import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeAll;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringSchemaTest {
-    private static String string;
-    private static String expected;
+    private  static String string;
+    private  String expected;
 
-    @BeforeAll
-    public static void beforeAll() {
-        var string = "what does the fox say";
-        var expected = "";
-    }
+//    @BeforeAll
+//    public static void beforeAll() {
+//        var string = "what does the fox say";
+//        var expected = "";
+//    }
 
     public static void schemaTest() {
         var result = "what does the fox say";
--- app/src/test/java/ValidatorTest.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-
-class ValidatorTest {
-    public static void ValidateTest(String a, String b) {
-        assertEquals(a, b);
-    }
-
-}
