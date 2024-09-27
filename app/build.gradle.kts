b6d7338a1026e791e21f0d6dd6c5a3d7305ff17c
salvadorrus <ntvkras@list.ru>
2024-09-27 17:53:10 +0300

    Fix workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,13 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     java
-    checkstyle
 //    application
+    checkstyle
+    jacoco
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.adarshr.test-logger") version "3.2.0"
 }
 
 //application {
@@ -18,11 +24,15 @@ repositories {
 dependencies {
 //    compileOnly("org.projectlombok:lombok:1.18.34")
 //    annotationProcessor("org.projectlombok:lombok:1.18.34")
-
+    implementation("org.apache.commons:commons-lang3:3.15.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.1")
+    testImplementation("org.assertj:assertj-core:3.26.3")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
--- app/src/main/java/hexlet/code/MapSchema.java
+++ app/src/main/java/hexlet/code/MapSchema.java
@@ -17,9 +17,9 @@ public class MapSchema<K, V> extends BaseSchema<Map<K, V>> {
 
     public MapSchema<K, V> shape(Map<K, BaseSchema<V>> schema) {
         readPredicate("shape", value -> schema.entrySet().stream().allMatch(item -> {
-                    Object object = ((Map<?, ?>) value).get(item.getKey());
-                    return item.getValue().isValid(object);
-                })
+            Object object = ((Map<?, ?>) value).get(item.getKey());
+            return item.getValue().isValid(object);
+        })
         );
         return this;
     }
