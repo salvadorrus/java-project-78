31027b2e30153a066cd38e8ab0da819f387ddee3
salvadorrus <ntvkras@list.ru>
2024-09-27 17:06:44 +0300

    Fix checkstyle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    id("java")
-    application
+    java
+    checkstyle
+//    application
 }
 
-application {
-    mainClass = "hexlet.code.App"
-}
+//application {
+//    mainClass = "hexlet.code.App"
+//}
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
@@ -15,11 +16,11 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
+//    compileOnly("org.projectlombok:lombok:1.18.34")
+//    annotationProcessor("org.projectlombok:lombok:1.18.34")
 
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.1")
 }
 
 tasks.test {
